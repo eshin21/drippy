158dda407568d86d77f522b0bf1a640d82444f5a
Enoch Shin <eshin135@gmail.com>
2026-04-07 22:54:31 +0200

    redoing images for the RC, adding diagonal scoring for RealSample_New_Metrics

--- RealSample_New_Metrics.py
+++ RealSample_New_Metrics.py
@@ -35,7 +35,7 @@ def _visualize_matrix(input_matrix, colorscheme, lowerbound, upperbound, title):
 ####################################################################################
 ## FILE I/O
 
-meme_file = "meme_out_2/meme.xml"
+meme_file = "IMPORTS/meme_out_2/meme.xml"
 
 
 ### Accessing direct sequences
@@ -147,7 +147,6 @@ info_content_res = H_before - after_df
 #IC Viz
 ####################################################################################
 
-
 _visualize_matrix(info_content_res, colorscheme='viridis_r', lowerbound=0, upperbound=2, title="Information")
 
 
@@ -199,11 +198,7 @@ for i in range(ppm.shape[1]):
            
             pearson_results_df.iloc[i, j] = pearsonr(x, y)[0]
 
-
-
 _visualize_matrix(input_matrix = pearson_results_df, colorscheme='viridis', lowerbound=0, upperbound=1, title="Pearson")
-
-
 
 ####################################################################################
 # New Metric
@@ -220,7 +215,53 @@ ic_jsd = info_content_res - jsd_results_df
 
 _visualize_matrix(ic_jsd, colorscheme='viridis', lowerbound=-1, upperbound=2, title="New Metric: Information - JSD")
 
+#####################################################################################
+# Diagonal scoring
+#####################################################################################
 
+def score_diagonals(matrix, threshold, direction='main'):
+    """
+    Find the longest diagonal (or anti-diagonal) run in the lower triangle
+    where all values >= threshold.
+    Parameters
+    ----------
+    matrix     : 2D numpy array (symmetric)
+    threshold  : float, minimum cell value to count as a hit
+    direction  : 'main' for diagonals parallel to the main diagonal,
+                 'anti' for anti-diagonals
+    Returns
+    -------
+    list of (i, j) tuples for the longest qualifying diagonal run,
+    expressed as (row, col) in the lower triangle
+    """
+    n = matrix.shape[0]
+    best_len = 0
+    best_coords = []
+
+    for start_i in range(n):
+        for start_j in range(start_i):      # strict lower triangle: j < i
+            if (start_i - start_j) < 1: #should we be checking identity diagonals for RCs?
+                continue
+            i = start_i
+            j = start_j
+            coords = []
+            while 0 <= i < n and 0 <= j < n:
+                if matrix[i, j] >= threshold:
+                    coords.append((i, j))
+                    i += 1
+                    j += 1 if direction == 'main' else -1
+                else:
+                    break
+            if len(coords) > 1 and len(coords) > best_len: #change this to append to save all candidates ? 
+                best_len = len(coords)
+                best_coords = coords
+    return best_coords
+
+ic_jsd_np = ic_jsd.to_numpy
+
+score_diagonals(ic_jsd, threshold=1.5, direction='main')
+
+ic_jsd.to_excel("scoring_ic_jsd.xlsx")
 
 ####################################################################################
 # New Metric
